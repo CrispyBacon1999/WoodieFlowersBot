83df5f93c2cdceb39cb23a7291b12c084fd4eb2e
Josh <bacon.josh09@gmail.com>
2017-07-07 19:46:00 -0400

    Updating to version 3.1 of the bot api

--- logger.py
+++ logger.py
@@ -1,9 +1,10 @@
 import botconfig
+import json
 
 PINK = '\033[95m'
 BLUE = '\033[94m'
 GREEN = '\033[92m'
-ORANGE = '\033[93m'
+YELLOW = '\033[93m'
 RED = '\033[91m'
 WHITE = '\033[0m'
 BOLD = '\033[1m'
@@ -20,20 +21,30 @@ def log(msg):
 
 # Log a message from a supergroup to print the message in a neat format
 def _log_supergroup(msg):
-    if 'last_name' in msg['from']:
-        print('%s%s%s - %s%s %s%s : %s%s%s' % (RED, msg['chat']['title'], PINK,
-                                               BLUE, msg['from']['first_name'],
-                                               msg['from']['last_name'], PINK,
-                                               WHITE, msg['text'], WHITE))
+    if 'text' in msg:
+        if 'last_name' in msg['from']:
+            print('%s%s%s - %s%s %s%s : %s%s%s' % (RED, msg['chat']['title'], PINK,
+                                                   BLUE, msg['from']['first_name'],
+                                                   msg['from']['last_name'], PINK,
+                                                   WHITE, msg['text'], WHITE))
+        else:
+            print('%s%s%s - %s%s%s : %s%s%s' % (RED, msg['chat']['title'], PINK,
+                                                   BLUE, msg['from']['first_name'], PINK,
+                                                   WHITE, msg['text'], WHITE))
 
 
 # Log a message from a private message to print it in a neat format
 def _log_private(msg):
-    if 'last_name' in msg['from']:
-        print('%s%s%s - %s%s %s%s : %s%s%s' % (RED, 'PRIVATE', PINK,
-                                               BLUE, msg['from']['first_name'],
-                                               msg['from']['last_name'], PINK,
-                                               WHITE, msg['text'], WHITE))
+    if 'text' in msg:
+        if 'last_name' in msg['from']:
+            print('%s%s%s - %s%s %s%s : %s%s%s' % (RED, 'PRIVATE', PINK,
+                                                   BLUE, msg['from']['first_name'],
+                                                   msg['from']['last_name'], PINK,
+                                                   WHITE, msg['text'], WHITE))
+        else:
+            print('%s%s%s - %s%s%s : %s%s%s' % (RED, 'PRIVATE', PINK,
+                                                BLUE, msg['from']['first_name'], PINK,
+                                                WHITE, msg['text'], WHITE))
 
 
 # Log an error message
@@ -43,4 +54,28 @@ def error(msg):
 
 # Log an initialization message
 def init_log(msg):
-    print(ORANGE + msg + WHITE)
+    print(YELLOW + msg + WHITE)
+
+
+# Log a temporary message, usually for testing purposes
+def temp_log(msg):
+    print(PINK + msg + WHITE)
+
+
+# Prints json message to log file. Usually used for testing purposes
+def log_to_file(msg):
+    f = open(botconfig.logfile, 'a')
+    f.write(json.dumps(msg, indent=4, sort_keys=True) + ',\n')
+    f.close()
+
+def log_callback(msg):
+    if 'text' in msg:
+        if 'last_name' in msg['from']:
+            print('%s%s%s - %s%s %s%s : %s%s%s' % (RED, 'CALLBACK', PINK,
+                                                   BLUE, msg['from']['first_name'],
+                                                   msg['from']['last_name'], PINK,
+                                                   WHITE, msg['text'], WHITE))
+        else:
+            print('%s%s%s - %s%s%s : %s%s%s' % (RED, 'CALLBACK', PINK,
+                                                BLUE, msg['from']['first_name'], PINK,
+                                                WHITE, msg['text'], WHITE))
--- woodie.py
+++ woodie.py
@@ -1,32 +1,108 @@
-import telepot
+from vyper import vyper
+from plugins import betatba, user, admin
 import botconfig
 import time
+import dbhandler
 import logger as l
+import re
+from plugins import xp
 
 
 def init():
-    l.init_log('Initializing Plugins')
-    for plugin in botconfig.plugins:
-        if callable(init):
-            l.init_log('Initializing plugin: %s' % plugin.command)
-            plugin.init()
+	l.init_log('Initializing Plugins')
+	for plugin in botconfig.plugins:
+		if callable(init):
+			l.init_log('Initializing plugin: %s' % plugin.command)
+			plugin.init()
+	botconfig.init_plugins(bot)
+	pluginnames = [plugin.__class__.__name__ for plugin in botconfig.plugins]
+	for plugin in pluginnames:
+		l.init_log(plugin + ': INITIALIZED')
+	botconfig.USERNAME = '@' + bot.getMe()['username']
 
 
 def chat(msg):
-    l.log(msg)
+	if 'text' in msg['message']:
+		l.log(msg['message'])
+		if 'username' in msg['message']['from']:
+		  dbhandler.refreshuser_id(msg['message']['from']['username'], msg['message']['from']['id'])
+		#xp.add_xp(msg['message']['from']['id'])
+		for plugin in botconfig.plugins:
+			plugin.test_command(msg['message'], 0)
 
 
 def inline_button(msg):
-    pass
-l.init_log('Starting Woodie Flowers - %s...' % botconfig.VERSION)
-init()
+	msg = msg['callback_query']
+	match = re.search('^(team|event)_', msg['data'])
+	if match:
+		for plugin in botconfig.plugins:
+			if isinstance(plugin, betatba.Tba):
+				message = {
+					'text': msg['data'],
+					'entities': [
+						{'type': 'callback'}],
+					'chat': {
+						'id': msg['message']['chat']['id'],
+						'type': msg['message']['chat']['type']
+					},
+					'message_id': msg['message']['message_id'],
+					'from': {
+						'first_name': msg['from']['first_name'],
+						'id': msg['from']['id']
+					},
+					'callback_id': msg['id']
+				}
+				plugin.execute(message)
+	match = re.search('^user_', msg['data'])
+	if match:
+		for plugin in botconfig.plugins:
+			if isinstance(plugin, user.User):
+				message = {
+					'text': msg['data'],
+					'entities': [
+						{'type': 'callback'}],
+					'chat': {
+						'id': msg['message']['chat']['id'],
+						'type': msg['message']['chat']['type']
+					},
+					'message_id': msg['message']['message_id'],
+					'from': {
+						'first_name': msg['from']['first_name'],
+						'id': msg['from']['id']
+					}
+				}
+				plugin.execute(message)
+	match = re.search('^admin_', msg['data'])
+	if match:
+		for plugin in botconfig.plugins:
+			if isinstance(plugin, admin.Admin):
+				message = {
+					'text': msg['data'],
+					'entities': [
+						{'type': 'callback'}],
+					'chat': {
+						'id': msg['message']['chat']['id'],
+						'type': msg['message']['chat']['type']
+					},
+					'message_id': msg['message']['message_id'],
+					'from': {
+						'first_name': msg['from']['first_name'],
+						'id': msg['from']['id']
+					}
+				}
+				plugin.execute(message)
+			
+			
+			
+			
+l.init_log(botconfig.logo)
+l.init_log('	  ---	Starting Woodie Flowers Bot - %s   ---' % botconfig.VERSION)
 l.init_log('Connecting to Telegram Servers with Bot Token...')
-bot = telepot.Bot(botconfig.TOKEN)
+bot = vyper.API().configure(botconfig.TOKEN, functions={'message': chat,
+														'callback_query': inline_button})
+init()
 l.init_log('Connected to Telegram Servers!')
 l.init_log('Starting webhook loop to receive new messages.')
-bot.message_loop({
-    'chat': chat,
-    'callback_query': inline_button
-})
 while True:
-    time.sleep(5)
+	bot.getUpdates()
+	time.sleep(.05)
